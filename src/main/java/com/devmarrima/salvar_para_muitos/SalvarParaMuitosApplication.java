98def68460d66d2b197fa51a214e9c39441194c7
curso_básico_Dio <marrimacelso@gmail.com>
2025-01-28 20:43:43 -0300

    primeiro commit

---
+++ src/main/java/com/devmarrima/salvar_para_muitos/SalvarParaMuitosApplication.java
@@ -0,0 +1,13 @@
+package com.devmarrima.salvar_para_muitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalvarParaMuitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalvarParaMuitosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmarrima/salvar_para_muitos/SalvarParaMuitosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmarrima.salvar_para_muitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalvarParaMuitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
